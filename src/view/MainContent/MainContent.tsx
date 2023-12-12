58e72da7cadb65283fa5c9cd6f2c9b2fc219204b
Sathin Thenuja <sathinthenuja@gmail.com>
2023-12-12 16:08:59 +0530

    navigation bar done

---
+++ src/view/MainContent/MainContent.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+export class MainContent extends Component {
+    render() {
+        return (
+            <div className="flex">
+                <div></div>
+                <div></div>
+            </div>
+        );
+    }
+}
